85f938fd943944fa9c7ea1e944ae67eba2b263fc
LechTheMitch <104312143+LechTheMitch@users.noreply.github.com>
2025-11-23 14:40:54 +0200

    Initial Knap

--- knapsack/app.py
+++ knapsack/app.py
@@ -1,1 +1,13 @@
-print("Hello World!")
+from enum import Enum, auto
+class ProblemType (Enum):
+    BOUNDED = auto()
+    UNBOUNDED = auto()
+class Knapsack:
+    def __init__(self, capacity: int, problemType: ProblemType):
+        if capacity <= 0:
+            raise ValueError("The Knapsack Capacity must be a positive integer")
+        self.capacity = capacity
+        self.problemType = problemType
+
+    def clear_sack(self) -> None:
+        self.items.clear()
