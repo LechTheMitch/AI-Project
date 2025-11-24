cf72a730b95d16a5170ef53cf5cc00a54a773d7f
LechTheMitch <104312143+LechTheMitch@users.noreply.github.com>
2025-11-24 22:38:56 +0200

    Should have commited sooner, no need to repeat the same mistake

--- knapsack/app.py
+++ knapsack/app.py
@@ -1,7 +1,29 @@
-from knapsack import Knapsack
+from knapsack import Knapsack as K, ProblemType
 
-weights = list(map(int, input("Enter the Weights Seprated by spaces ").split(" ")))
-values = list(map(int, input("Enter the Values Seprated by spaces ").split(" ")))
+def get_knapvalues(itemname: str):
+    while True:
+        try:
+            knapvalues = list(map(int, input(f"Enter the {itemname} Seprated by spaces ").split()))
 
-while len(values) != len(weights):
-    values = list(map(int, input("Please enter the same number of values as waits ").split(" ")))
+            if any(knapvalue <= 0 for knapvalue in knapvalues):
+                print(f"All {itemname.lower()} must be positive integers")
+                continue
+        
+            return knapvalues
+
+        except ValueError:
+            print(f"{itemname} must be positive integers")
+
+capacity = int(input("Please enter the Sack's capacity "))
+weights = get_knapvalues("Weights")
+
+while True:
+    values = get_knapvalues("Values")
+    
+    if len(weights) == len(values):
+        break
+
+    print("There must be as many values as there are weights")
+
+s = K(ProblemType.BOUNDED, capacity, weights, values)
+s.test_print(weights[0])
--- knapsack/knapsack.py
+++ knapsack/knapsack.py
@@ -5,19 +5,67 @@ class ProblemType (Enum):
     BOUNDED = auto()
     UNBOUNDED = auto()
 
-# Need to inquire about the number of generations and the mutation rates, whether they are user determined or I can skip that step
 class Knapsack:
+    POP_SIZE = 100
+    MUTATION_RATE = 0.1
+
     def __init__(self, problem_type: ProblemType, capacity: int, weights, values): # Weights and Values need Type Checking
         if capacity <= 0:
             raise ValueError("The Knapsack Capacity must be a positive integer")
         self.capacity = capacity
         self.problem_type = problem_type
         self.num_of_items = len(weights)
-        self.current_population = [] # Will used as state, similar to React
+        self.current_population = [] # Will be used as state, similar to React
+        self.weights = np.array(weights)
+        self.values = np.array(values)
+        self.best_solution = None #Elitism
+        self.best_fitness = 0
+        self.normative_bounds = [np.zeros(self.num_of_items).astype(int), np.array([self.calculate_maxitem(weight) for weight in self.weights])]
 
+    def create_population(self):
+        random_percentages = np.random.rand(self.POP_SIZE, self.num_of_items)
+        itemcounts = random_percentages * (self.normative_bounds[1] + 1) # Note to self, the +1 here is to alleviate the issue of not being able to reach the maximum value as .rand never generate a 1.0 limit is 0.99
+        return np.array(itemcounts).astype(int) #The acutal number of items we could have inside the sack
 
-    @classmethod
     def clear_sack(self) -> None:
-        self.items.clear()
-        
+        self.current_population.clear()
+
+    def calculate_maxitem(self, weight) -> int: # only relevant for unbounded
+        return self.capacity // weight
     
+    def calculate_fitness(self, population):
+        total_weight = np.dot(population, self.weights)
+        total_value = np.dot(population, self.values)
+        fitness = np.where(total_weight <= self.capacity, total_value, 0)
+        return fitness, total_weight
+    
+    # def update_belief_space(self, population, fitness):
+    #     current_best_idx = np.argmax(fitness)
+    #     if fitness[current_best_idx] > self.best_fitness:
+    #         self.best_fitness = fitness[current_best_idx]
+    #         self.best_solution = population[current_best_idx].copy()
+
+    #     # 2. Update Normative Knowledge (Acceptable Ranges)
+    #     # We take the top 20% of the population to define "good" ranges
+    #     top_percentile_idx = np.argsort(fitness)[-int(self.pop_size * 0.2):]
+    #     top_performers = population[top_percentile_idx]
+        
+    #     self.normative_bounds[0] = np.min(top_performers, axis=0)
+    #     self.normative_bounds[1] = np.max(top_performers, axis=0)
+
+    def mutate(self, individual):
+        for i in range(self.n_items):
+            if np.random.rand() < self.MUTATION_RATE:
+                low = int(self.normative_bounds[0][i])
+                high = int(self.normative_bounds[1][i])
+                
+                if low < high:
+                    individual[i] = np.random.randint(low, high + 1)
+                else:
+                    individual[i] = low
+        return individual
+    
+    def test_print(self, weight): # TODO needs to be removed later
+        self.current_population = self.create_population()
+        print(self.calculate_fitness(self.current_population))
+
